cdaf91223479fe16160946474d88a815bc6e40d7
vadxq <dxl@vadxq.com>
2022-07-31 10:04:25 +0800

    feat: add tialwindcss plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,10 @@ module.exports = {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp')
+  ]
 };
